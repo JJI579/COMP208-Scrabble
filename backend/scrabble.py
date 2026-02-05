3bf88ae397f71848fba1425cc3dda9a62a72b088
JJI579 <jason.ivanovs@hotmail.com>
2026-02-05 22:41:39 +0000

    made mid word work

--- backend/scrabble.py
+++ backend/scrabble.py
@@ -99,31 +99,82 @@ class Scrabble:
 
 		return coordinates
 
-	def place_word(self, word: str, position: tuple[int, int], direction: str, blanks: list[int]):
+	def place_word(self, word: str, position: tuple[int, int], direction: str, blanks: list[int] = [], preExisting: list[tuple[int, int]] = []):
 		wordCoordinates = []
 		x = position[0]
 		y = position[1]
+		print("NEW ONE")
+		tempPlaced = []
 		for i in range(len(word)):
-			wordCoordinates.append([x, y])
-			if self.get_cell(x, y) != defaultFiller:
+			if [x,y] not in preExisting:
+				wordCoordinates.append([x, y])
+			cellContents = self.get_cell(x, y)
+			print('-------------\n' + word)
+			print(preExisting)
+			print([x,y ] in preExisting)
+			if cellContents != defaultFiller and [x, y] not in preExisting:
+				print(x, y)
+				print(self.get_cell(x, y))
 				print("cell has already been taken!")
 				# TODO: check if there is a word that works otherwise raise an issue.
+				for x, y in tempPlaced:
+					self.game[y][x] = defaultFiller
 				return
 			else:
 				# make it place already to imply that it can be used!
-				self.game[y][x] = word[i]
+				if [x, y] not in preExisting:
+					print("temp placed letter: ", word[i])
+					self.game[y][x] = word[i]
+					tempPlaced.append([x, y])
+				else:
+					print("missed letter: ", word[i])
 			if direction=="down":
 				y+=1
 			else:
 				x+=1
 		
-		potentialWord = self.expand_vertically(position)
-		if direction == "down":
-			potentialWord.sort(key=lambda x: x[1] )
-		else:
-			potentialWord.sort(key=lambda x: x[0] )
-		wordOrdered = [[x, self.get_cell(x[0], x[1])] for x in potentialWord]
-		print(wordOrdered)
+
+		# we need to implement below on every letter, however if the direction of the letter is going down, then you only need to consider horizontal for each letter
+		# and if the word placed was right, considered up and down along every letter, then see how that goes...
+
+		# below works for basic concatenation
+
+		# assume there is no joining word till proved
+		hasJoiningWord = False
+		# assume every connection is a word until proven wrong
+		isWord = True
+		for testDirection in ['right', "down"]:
+			if testDirection == "right":
+				potentialWord = self.expand_horizontally(position)
+			else:
+				potentialWord = self.expand_vertically(position)
+			testArray = potentialWord.copy()
+			# print("1")
+			# print(potentialWord)
+			[testArray.remove(x) for x in wordCoordinates if x in testArray]
+			print(potentialWord)
+			print(testArray)
+			if len(testArray) != 0:
+				if testDirection == "down":
+					potentialWord.sort(key=lambda x: x[1] )
+				else:
+					potentialWord.sort(key=lambda x: x[0] )
+				wordOrdered = [[x, self.get_cell(x[0], x[1])] for x in potentialWord]
+				wordString = ''.join([x[1] for x in wordOrdered])
+				print(f'string: ' + wordString)
+				if not self.check_word(wordString):
+					isWord = False
+				else:
+					print("this is true¬")
+					hasJoiningWord = True
+			else:
+				if not self.firstPlaced:
+					hasJoiningWord = True
+					self.firstPlaced = True
+		if not hasJoiningWord:
+			# remove coordinates placed
+			for x, y in tempPlaced:
+				self.game[y][x] = defaultFiller
 		# for l in wordOrdered:
 		# 	print(l)
 
@@ -154,12 +205,13 @@ class Scrabble:
 scrab = Scrabble([0, 1], arr)
 
 scrab.place_word("bet", (7,7), "down", [])
-# scrab.test_word("bet", (7,7), "down", [])
 scrab.place_word("ee", (8,7), "right", [])
-# scrab.test_word("te", (9,5), "down", [])
-# scrab.test_word("tit", (3,4), "down", [])
-# scrab.test_word("gap", (2,1), "down", [])
-# scrab.test_word("t", (3,4), "down", [])
+scrab.place_word("bet", (9,6), "down", preExisting=[[9,7]])
+
+# scrab.place_word("b", (9,6), "down", [])
+# scrab.place_word("t", (10,7), "down", [])
+# scrab.place_word("ea", (10,8), "down", [])
+# scrab.place_word("t", (10,10), "down", [])
 
 scrab.print_board()
 
