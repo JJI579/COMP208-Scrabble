cfb5202a1d4ba8402909343e306cd84bf53afa86
JJI579 <jason.ivanovs@hotmail.com>
2026-02-16 11:18:10 +0000

    made basic page before implementing functions to backend

---
+++ frontend/src/Components/Shop/UnlockedItem.vue
@@ -0,0 +1,69 @@
+<script lang="ts" setup>
+import type { UnlockedItemType } from '@/types';
+import type { PropType } from 'vue';
+
+
+
+const props = defineProps({
+	item: {
+		type: Object as PropType<UnlockedItemType>,
+		required: true,
+	}
+});
+const emit = defineEmits(['equip', "unequip"])
+const item = props.item;
+
+
+function equip() {
+	// TODO: implement
+}
+
+function unequip() {
+	// TODO: implement
+}
+
+function toggleEquip() {
+	if (item.equipped) {
+		unequip()
+		emit("unequip", props.item.itemID)
+	} else {
+		equip()
+		emit("equip", props.item.itemID)
+	}
+
+
+}
+</script>
+
+
+
+<template>
+
+	<div class="item">
+		<div class="item__name">
+			{{ item.name }} - <span v-if="item.equipped" class="item--unequip">[UNEQUIP]</span> <span
+				class="item--equip" v-else>[EQUIP]</span>
+		</div>
+		<div class="item__description">
+			{{ item.description }}
+		</div>
+	</div>
+</template>
+
+
+<style lang="css" scoped>
+.item {
+	display: flex;
+	flex-direction: column;
+	gap: .5rem;
+	justify-content: left;
+}
+
+.item__name {}
+
+.item--unequip {}
+
+.item--equip {}
+
+.item__description {}
+</style>
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -14,9 +14,21 @@ type UserReturn = {
 	userCreatedAt: string
 }
 
+type Item = {
+	itemID: number,
+	name: string,
+	description: string,
+	xpRequired: number,
+	unlocked: boolean
+}
+
+type UnlockedItemType = Item & {
+	equipped: boolean
+}
+
 type SelfReturn = UserReturn & {
 	friends: UserReturn[];
 }
 
-export type { LoginReturn, UserReturn, SelfReturn };
+export type { LoginReturn, UserReturn, SelfReturn, Item, UnlockedItemType };
 export default debug;
