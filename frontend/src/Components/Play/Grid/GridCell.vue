572b2df40782fdd95de5828bc69ab60d9f256601
Hari Sharma <shamahari123@gmail.com>
2026-02-17 18:51:46 +0000

    Designed scrabble grid

--- frontend/src/Components/Play/Grid/GridCell.vue
+++ frontend/src/Components/Play/Grid/GridCell.vue
@@ -14,6 +14,14 @@ const props = defineProps({
 	isDraft: {
 		type: Boolean,
 		required: true
+	},
+	x: {
+		type: Number,
+		required: true
+	},
+	y: {
+		type: Number,
+		required: true
 	}
 })
 
@@ -22,52 +30,65 @@ const score = computed(() => {
 	return pointsMap[letter] || ' ';
 })
 
+// center highlight
+const bgColour = computed(() => {
+	if (props.x === 7 && props.y === 7) {
+		return '#FFD700';
+	}
+	return '#8ED6FF';
+})
 
 </script>
 
 
 
 <template>
-	<div class="cell" :class="{ 'cell--draft': props.isDraft }">
-		<p class="letter">{{ props.cellValue.toUpperCase() }}</p>
-		<p class="score">{{ score }}</p>
+	<div class="cell" :class="{ 'cell--draft': isDraft }" :style="{ backgroundColor: bgColour }">
+		<p class="letter">{{ cellValue !== '|' ? cellValue.toUpperCase() : '' }}</p>
+    	<p class="score" v-if="cellValue !== '|'">{{ score }}</p>
 	</div>
 </template>
 
 
 <style lang="css" scoped>
 .cell {
-	user-select: none;
-	background-color: var(--tile-background-colour);
-	color: var(--tile-text-colour);
-	aspect-ratio: 1/1;
-	border: 2px solid var(--scrabble-board);
-	display: flex;
-	height: 100%;
-	width: 100%;
-	position: relative;
-	/* border-radius: 8px; */
+user-select: none;
+aspect-ratio: 1/1;
+border: 1px solid white;
+display: flex;
+justify-content: center;
+align-items: center;
+position: relative;
+font-weight: bold;
+font-size: 20px;
+color: #000; 
+}
+
+.cell:hover {
+  transform: scale(1.05);
 }
 
 .cell--draft {
-	color: grey;
+	opacity: .5;
 }
 
 .letter {
+	font-size: 24px;
+	max-width: 90%;
+	max-height: 90%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	position: absolute;
 	top: 50%;
 	left: 50%;
 	transform: translate(-50%, -50%);
-	margin: 0;
-	font-size: 24px;
-	font-weight: bolder;
 }
 
 .score {
-	position: absolute;
-	right: 0;
-	bottom: 0;
-	margin: 0;
-	transform: translate(-15%, -5%);
+  position: absolute;
+  right: 4px;
+  bottom: 2px;
+  font-size: 12px;
 }
 </style>
--- frontend/src/Components/Play/Grid/ModifierCell.vue
+++ frontend/src/Components/Play/Grid/ModifierCell.vue
@@ -10,24 +10,41 @@ const props = defineProps({
 })
 
 const convertedLabel = computed(() => {
-	if (props.modifier == "DOUBLE_LETTER") {
-		return "DL"
-	} else if (props.modifier == "TRIPLE_LETTER") {
-		return "TL"
-	} else if (props.modifier == "DOUBLE_WORD") {
-		return "DW"
-	} else if (props.modifier == "TRIPLE_WORD") {
-		return "TW"
+	switch (props.modifier) {
+		case 'DOUBLE_LETTER':
+			return 'Double Letter';
+		case 'TRIPLE_LETTER':
+			return 'Triple Letter';
+		case 'DOUBLE_WORD':
+			return 'Double Word';
+		case 'TRIPLE_WORD':
+			return 'Triple Word';
+		default:
+			return '';
 	}
 })
 
+const bgColour = computed(() => {
+	switch (props.modifier) {
+		case 'DOUBLE_LETTER':
+			return '#00008B';
+		case 'TRIPLE_LETTER':
+			return '#FF1493';
+		case 'DOUBLE_WORD':
+			return '#FFD700';
+		case 'TRIPLE_WORD':
+			return '#00A86B';
+		default:
+			return '#8ED6FF';
+	}
+})
 
 </script>
 
 
 
 <template>
-	<div class="modifier_cell">
+	<div class="modifier_cell" :style="{ backgroundColor: bgColour }">
 		<p class="letter">{{ convertedLabel }}</p>
 	</div>
 
@@ -37,30 +54,33 @@ const convertedLabel = computed(() => {
 <style lang="css" scoped>
 .modifier_cell {
 	user-select: none;
-	background-color: black;
 	aspect-ratio: 1/1;
-	border: 2px solid var(--scrabble-board);
-	background-color: #fff0cf;
+	border: 2px solid white;
 	display: flex;
 	justify-content: center;
 	align-items: center;
+	text-align: center;
 	height: 100%;
 	width: 100%;
 	position: relative;
-	/* border-radius: 8px; */
+	font-weight: bold;
+	font-size: 12px;
+	line-height: 1;
+	color: white;
+	box-shadow: 1px 1px 3px rbga(0, 0, 0, 0.3);
+	transition: transform 0.2s;
+}
+
+.modifier_cell:hover {
+	transform: scale(1.05);
 }
 
 .letter {
 	margin: 0;
 	padding: 0;
-	font-weight: bold;
 }
 
 .score {
-	position: absolute;
-	right: 0;
-	bottom: 0;
-	margin: 0;
-	transform: translate(-15%, -5%);
+
 }
 </style>
